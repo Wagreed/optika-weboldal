d31b0e2f26a1c78a089aaabf6a98d815d167357f
Wagreed <70488675+Wagreed@users.noreply.github.com>
2025-08-02 22:14:39 +0300

    Finished for today

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-blue-50 p-8">
+  <div class="min-h-screen bg-white-50 p-8 absolute inset-0">
     <h1 class="text-4xl font-bold text-blue-900 mb-4">
       Optika Fooldal
     </h1>
