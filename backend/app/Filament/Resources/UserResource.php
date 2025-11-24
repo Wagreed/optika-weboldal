decd8ee6f582d8ebdc7c473057fda0ddb7934396
Wagreed <70488675+Wagreed@users.noreply.github.com>
2025-11-24 13:30:53 +0200

    filament no_pass_edit for safety

--- backend/app/Filament/Resources/UserResource.php
+++ backend/app/Filament/Resources/UserResource.php
@@ -31,7 +31,9 @@ class UserResource extends Resource
                 Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
                     ->password()
-                    ->required(),
+                    ->required()
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->hiddenOn('edit'),
                 Forms\Components\TextInput::make('phone')
                     ->tel(),
                 Forms\Components\DatePicker::make('birth_date'),
